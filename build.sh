9699e3655500066e4e1b42b88172497122e33a80
Massimo Santini <massimo.santini@gmail.com>
2019-04-05 22:34:14 +0200

    Added index.html

--- build.sh
+++ build.sh
@@ -2,10 +2,31 @@
 
 rm -rf html
 mkdir html
+cp *css *js html
+cat > html/index.html <<EOF
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diario</title>
+    <link rel="stylesheet" href="style.css">
+    <script>const signature = '{}'</script>
+    <script src="main.js"></script>
+</head>
+<body>
+<ul>
+EOF
 for w in {00..52}; do
     if [ -r diario-$w.yaml ]; then
         ./format $w
+        echo "<li><a href='diario-$w.html'>$w</a></li>" >> html/index.html
     fi
 done
-cp *css *js html
 mv *.html html
+
+cat >> html/index.html <<EOF
+</ul>
+</body>
+</html>
+EOF
