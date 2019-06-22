749212934d2cbdfef1306373751bd38fe3c76782
Massimo Santini <massimo.santini@gmail.com>
2019-06-22 11:21:10 +0200

    Mod x vacanze

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ cat > html/index.html <<EOF
 <table class='table'>
 EOF
 n=0
-for w in {00..52}; do
+for w in {00..23} vacanze; do
     if [[ $(($n % 6)) = 0 ]]; then
         echo "<tr>" >> html/index.html
     fi
